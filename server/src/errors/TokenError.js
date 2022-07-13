9e0e7667cc8f7cc9041f223f569fc88e17f646fc
Kseniia98 <kseniia.plakhotnia@gmail.com>
2022-07-14 00:10:57 +0300

    Add refresh token

--- server/src/errors/TokenError.js
+++ server/src/errors/TokenError.js
@@ -2,8 +2,7 @@ const ApplicationError = require('./ApplicationError');
 
 class TokenError extends ApplicationError{
   constructor (message) {
-    super(message || 'token error', 408);
-  }
+    super(message || 'token error', 401);  }
 }
 
 module.exports = TokenError;
